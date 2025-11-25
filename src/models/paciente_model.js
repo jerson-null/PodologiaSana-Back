91f78bb17f8adc60933bc624ec2c5848c654eaec
Ashley <ashley.bayonav@gmail.com>
2025-11-25 17:17:43 -0500

    testeo

--- src/models/paciente_model.js
+++ src/models/paciente_model.js
@@ -138,5 +138,6 @@ exports.getPacienteByIdentificacion = async (identificacion) => {
         SELECT * FROM paciente WHERE identificacion = ?
     `;
     const [result] = await db.query(query, [identificacion]);
+    console.log(result);
     return result[0] || null;
 }
